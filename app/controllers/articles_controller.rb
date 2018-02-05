d93f164be323331cc18f78a3ff85a99d84bc3e96
coadyduffney <coadyduffney@gmail.com>
2018-02-05 23:16:46 +0000

    Add admin user functionaly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   
   def destroy 
     @article.destroy
-    flash[:danger] = "Article was succesfully deleted"
+    flash[:success] = "Article was succesfully deleted"
     redirect_to articles_path
   end
   
@@ -62,7 +62,7 @@ class ArticlesController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "You may only make changes to your own articles"
       redirect_to root_path
     end
